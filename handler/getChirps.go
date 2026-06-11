ca2f763cbe682ee60ffd43444d715fe61a02a5b7
Johann Schneider <joschi.dev7@gmail.com>
2026-06-11 19:07:37 +0200

    feat: add query params, like sorting or author_id

--- handler/getChirps.go
+++ handler/getChirps.go
@@ -1,14 +1,48 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
+	"github.com/joschi64/Go_HTTP_Server/internal/database"
+)
 
 func (a *ApiConfig) HandleChirps(w http.ResponseWriter, r *http.Request) {
 
-	chirps, err := a.DB.GetChirps(r.Context())
+	author_id := r.URL.Query().Get("author_id")
+	sortT := r.URL.Query().Get("sort")
 
-	if err != nil {
-		respondWithError(w, 500, "Error with getting all chirps")
-		return
+	chirps := make([]database.Chirp, 0)
+
+	if author_id != "" {
+		userId, err := uuid.Parse(author_id)
+
+		if err != nil {
+			respondWithError(w, 400, "error parsing id")
+		}
+
+		chirps, err = a.DB.GetChirpsFromUser(r.Context(), uuid.NullUUID{
+			UUID:  userId,
+			Valid: true,
+		})
+
+		if err != nil {
+			respondWithError(w, 500, "Error with getting all chirps")
+			return
+		}
+	} else {
+		var err error
+		chirps, err = a.DB.GetChirps(r.Context())
+
+		if err != nil {
+			respondWithError(w, 500, "Error with getting all chirps")
+			return
+		}
+	}
+
+	if sortT == "desc" {
+		sort.Slice(chirps, func(i, j int) bool { return chirps[i].CreatedAt.After(chirps[j].CreatedAt) })
 	}
 
 	payload := make([]responseChirp, 0, len(chirps))
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -12,6 +12,12 @@ RETURNING *;
 -- name: GetChirps :many
 SELECT * FROM chirps ORDER BY created_at;
 
+-- name: GetChirpsFromUser :many
+SELECT * 
+FROM chirps
+WHERE user_id = $1
+ORDER BY created_at;
+
 -- name: GetChirp :one
 SELECT * 
 FROM chirps
